66c89442074591c43467e52ab6cb05bb49431924
蔡灼炫 <14753716+cai-zhuoxuan@user.noreply.gitee.com>
2025-08-13 18:12:17 +0800

    fix: 文档目录重命名

--- docs/sideBar.ts
+++ docs/sideBar.ts
@@ -5,8 +5,8 @@ export default {
             { 
               text: '响应式原理', 
               items: [
-                { text: '1. 响应式系统', link: '1-reactive.md' },
-                { text: '2. 依赖收集', link: '2-effect.md' }
+                { text: '1. reactive', link: '1-reactive.md' },
+                { text: '2. effect', link: '2-effect.md' }
               ]
             }
           ]
