285be612e107695dca3f962df36c401ad6b358b2
蔡灼炫 <14753716+cai-zhuoxuan@user.noreply.gitee.com>
2025-08-18 16:16:26 +0800

    doc: scheduler文档整理

--- docs/sideBar.ts
+++ docs/sideBar.ts
@@ -5,10 +5,11 @@ export default {
             { 
               text: '1-reactivity', 
               items: [
-                { text: '1. reactive', link: '1-reactive.md' },
-                { text: '2. effect', link: '2-effect.md' },
+                { text: '1. reactive', link: '1-reactive' },
+                { text: '2. effect', link: '2-effect' },
                 { text: '3. 依赖收集 & 触发更新', link: '3-依赖收集&触发更新' },
-                { text: '4. 依赖清理', link: '4-依赖清理' }
+                { text: '4. 依赖清理', link: '4-依赖清理' },
+                { text: '4. effect调度', link: '5-scheduler' }
               ]
             }
           ]
