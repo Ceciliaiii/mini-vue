00ab542d92f8730291ec236ff8ca467d367e07b0
蔡灼炫 <14753716+cai-zhuoxuan@user.noreply.gitee.com>
2025-08-18 11:42:39 +0800

    doc: 优化文档

--- docs/sideBar.ts
+++ docs/sideBar.ts
@@ -7,7 +7,7 @@ export default {
               items: [
                 { text: '1. reactive', link: '1-reactive.md' },
                 { text: '2. effect', link: '2-effect.md' },
-                { text: '3. 依赖收集', link: '3-依赖收集' },
+                { text: '3. 依赖收集 & 触发更新', link: '3-依赖收集&触发更新' },
                 { text: '4. 依赖清理', link: '4-依赖清理' }
               ]
             }
