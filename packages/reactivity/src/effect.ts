d45cb48433e56b30de3851396a016afd9d46d962
蔡灼炫 <14753716+cai-zhuoxuan@user.noreply.gitee.com>
2025-08-20 09:22:57 +0800

    fix: 修正trackEffect与其文档

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -122,11 +122,11 @@ export function trackEffect(effect, dep) {
   }
 
 
-  dep.set(effect, effect._trackId)
+  // dep.set(effect, effect._trackId)
 
   // effect和dep关联起来
   // effect.deps.push(dep)
-  effect.deps[effect._depsLength++] = dep
+  // effect.deps[effect._depsLength++] = dep
 }
 
 
